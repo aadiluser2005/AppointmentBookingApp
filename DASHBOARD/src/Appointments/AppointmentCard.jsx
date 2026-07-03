02b628d660fcbf0cc9a2b9910e0b49960b0938b1
aadiluser2005 <aadiluser2005@gmail.com>
2026-07-03 21:01:49 +0530

    Fixed IST date issue

--- DASHBOARD/src/Appointments/AppointmentCard.jsx
+++ DASHBOARD/src/Appointments/AppointmentCard.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
 
-function AppointmentCard({ patientName,email, date,slotTime, bookingDate, status,isOld,isHeader,cancel}) {
+function AppointmentCard({ patientName,email,date,slotTime, bookingDate, status,isOld,isHeader,cancel}) {
+  var istDate="";
+   //converting dateBooked to IST date for better accuracy
+   if(isHeader==false){
+      const tempDate = new Date(bookingDate);
+
+ istDate = new Intl.DateTimeFormat("en-CA", {
+  timeZone: "Asia/Kolkata",
+}).format(tempDate);
+// console.log(istDate);
+   }
+
+
+
+
+
 // console.log(isOld);
   return (
     <div className={`appointmentCard  ${isHeader?"text-muted":""}`}>
@@ -8,7 +23,7 @@ function AppointmentCard({ patientName,email, date,slotTime, bookingDate, status
           <div style={{maxWidth:"100rem", minWidth:"13rem" ,textAlign:"left"}}>{email}</div>
           <div style={{width:"80%" ,textAlign:"center"}}>{isHeader?date:date.slice(0,10)}</div>
           <div style={{width:"60%" ,textAlign:"center"}}>{slotTime}</div>
-          <div  style={{width:"88%" ,textAlign:"center"}}>{isHeader?bookingDate:bookingDate.slice(0,10)}</div>
+          <div  style={{width:"88%" ,textAlign:"center"}}>{isHeader?bookingDate:istDate}</div>
           <div style={{width:"80%" ,textAlign:"center"}}>{isHeader?status:status?<div className='confirmCapsule'>confirmed</div>:<div className='cancelCapsule'>cancelled</div>}</div>
           <div style={{width:"70%",  pointerEvents:`${(!isOld&&status)?"":"none"}` ,textAlign:"center"}} onClick={isHeader?"":cancel} >{isHeader?"Action":<i className="fa-solid fa-xmark ms-1"  title="Cancel Appointment" style={{color:"red"}}></i>}</div>
            
